11b63f8a7af56344375ab1a8eecf622ac1403c2f
Joel Crosby <JoelCrosby@live.co.uk>
2019-01-08 21:39:25 +0000

    format

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-extern crate serde_json;
 extern crate mime_guess;
+extern crate serde_json;
 
 #[macro_use]
 extern crate serde_derive;
 
+use std::ffi::OsStr;
 use std::fs::File;
-use std::io::prelude::*;
 use std::io;
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::path::Path;
-use std::ffi::OsStr;
 
 #[derive(Serialize, Deserialize)]
 struct Options {
@@ -19,7 +19,6 @@ struct Options {
 }
 
 fn main() {
-    
     let o = load_configuration().unwrap();
 
     println!("\nNano server started!");
@@ -32,22 +31,22 @@ fn main() {
         Err(error) => {
             println!("\nNano server was unable to start {}", error);
             return;
-        },
+        }
     };
 
     println!("\nListening for connections...\n");
 
     for stream in listener.incoming() {
-            match stream {
+        match stream {
             Ok(stream) => {
                 handle_connection(stream, &o.wwwroot);
             }
-            Err(e) => println!("Connection failed... {}", e)
+            Err(e) => println!("Connection failed... {}", e),
         }
     }
 }
 
-fn load_configuration() -> Result<Options, io::Error>  {
+fn load_configuration() -> Result<Options, io::Error> {
     let path = "nano.json".to_string();
     let config = read_file(&path).unwrap();
 
@@ -57,18 +56,17 @@ fn load_configuration() -> Result<Options, io::Error>  {
 }
 
 fn handle_connection(mut stream: TcpStream, wwwroot: &String) {
-
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
     let strget = String::from_utf8_lossy(&buffer);
-    
 
-    let chunks:Vec<_> = strget.split_whitespace().collect();
-    let getfile = chunks[1].replace("/", "\\")
-        .trim_left_matches("\\").to_string();
-
+    let chunks: Vec<_> = strget.split_whitespace().collect();
+    let getfile = chunks[1]
+        .replace("/", "\\")
+        .trim_start_matches("\\")
+        .to_string();
 
     let mainpage = "index.html".to_string();
 
@@ -77,18 +75,26 @@ fn handle_connection(mut stream: TcpStream, wwwroot: &String) {
     let ext = get_extension_from_filename(&getfile);
     let ext = match ext {
         Some(val) => val,
-        None => ".html"
+        None => ".html",
     };
 
     let (status_line, filename, mime) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n", &mainpage, mime_guess::get_mime_type_str(&mainpage))
+        (
+            "HTTP/1.1 200 OK\r\n",
+            &mainpage,
+            mime_guess::get_mime_type_str(&mainpage),
+        )
     } else {
-        ("HTTP/1.1 200 OK\r\n", &getfile, mime_guess::get_mime_type_str(ext))
+        (
+            "HTTP/1.1 200 OK\r\n",
+            &getfile,
+            mime_guess::get_mime_type_str(ext),
+        )
     };
 
     let mime = match mime {
         Some(val) => val,
-        None => "text/html;"
+        None => "text/html;",
     };
 
     let mut w = wwwroot.clone();
@@ -102,7 +108,12 @@ fn handle_connection(mut stream: TcpStream, wwwroot: &String) {
     };
 
     if res.is_ok() {
-        let response = format!("{}Content-Type: {}\r\n\r\n{}", status_line, mime, res.unwrap());
+        let response = format!(
+            "{}Content-Type: {}\r\n\r\n{}",
+            status_line,
+            mime,
+            res.unwrap()
+        );
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     } else {
@@ -114,13 +125,12 @@ fn handle_connection(mut stream: TcpStream, wwwroot: &String) {
 
 fn read_file(filename: &String) -> Result<String, io::Error> {
     let f = File::open(filename);
-        
-    let mut f = match f {
 
+    let mut f = match f {
         Ok(file) => file,
         Err(error) => {
             return Err(error);
-        },
+        }
     };
 
     let mut contents = String::new();
@@ -131,7 +141,5 @@ fn read_file(filename: &String) -> Result<String, io::Error> {
 }
 
 fn get_extension_from_filename(filename: &str) -> Option<&str> {
-    Path::new(filename)
-        .extension()
-        .and_then(OsStr::to_str)
+    Path::new(filename).extension().and_then(OsStr::to_str)
 }
